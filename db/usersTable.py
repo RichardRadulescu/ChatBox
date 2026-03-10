a25e54478c617ac5b007a06f2b1a4df7153271d7
RichardRadulescu <Richard.Radulescu@cognizant.com>
2026-03-10 14:41:30 +0200

    fixed db connection and routes

--- db/usersTable.py
+++ db/usersTable.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase, relationship
-from db.userFilesTable import user_file_link
+from sqlalchemy.orm import relationship
+from db.database import Base, user_file_link
 
 
-class UsersTable(DeclarativeBase):
-    __tablename__ = "users"
+class UsersTable(Base):
+    __tablename__ = "Users"
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
+    first_name = Column("firstName", String, nullable=False)
+    last_name = Column("lastName", String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    users = relationship("UserTable", secondary=user_file_link, back_populates="files")
+    hashed_password = Column("hashedPassword", String, nullable=False)
+    files = relationship(
+        "FilesTable", secondary=user_file_link, back_populates="owners"
+    )
--- routes/userRoutes.py
+++ routes/userRoutes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db.usersTable import UsersTable
+from models.file import FileRead, FileWrite
 from models.user import UserRead, UserWrite
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -13,7 +14,7 @@ def create_user(user: UserWrite, db: Session = Depends(get_db)):
         first_name=user.first_name,
         last_name=user.last_name,
         email=user.email,
-        hashed_password=user.password + "notsecurehash",  # Hash this properly later!
+        hashed_password=user.password,
     )
     db.add(db_user)
     db.commit()
@@ -27,3 +28,8 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+
+@router.post("/{user_id}/files", response_model=FileRead)
+def create_user_file(user_id: int, file_data: FileWrite, db: Session = Depends(get_db)):
+    pass
